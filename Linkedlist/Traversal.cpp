85a12fcf8a5791140b7755a45bd41ca33c6b2e7f
Ayushi0405 <aayushi.gupta0405@gmail.com>
2025-03-16 23:42:23 +0530

    LL

--- Linkedlist/Traversal.cpp
+++ Linkedlist/Traversal.cpp
@@ -29,7 +29,50 @@ Node* convertArr2ll(vector<int> & arr){
     return head;
 
 }
-int length
+int lengthofLL(Node* head){
+    int cnt=0;
+    Node* temp=head;
+    while(temp){
+        
+        temp=temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+int checkIfPresent(Node* head,int val){
+    Node* temp=head;
+    while(temp){
+        if(temp->data==val){
+            return 1;
+
+        }
+        temp=temp->next;
+    }
+    return 0;
+}
+Node* removesHead(Node* head){
+    if(head==NULL) return head;
+    Node* temp=head;
+    head=head->next;
+    delete temp;
+    return head;
+}
+
+Node* Deletetail(Node* head){
+    
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* temp=head;
+    while(temp->next->next !=NULL){
+        temp=temp->next;
+    }
+    delete temp->next;
+    temp->next=nullptr;
+
+    return head;
+}
+
+
     int main(){
         vector<int> arr={2,5,8,7};
         Node* head=convertArr2ll(arr);
@@ -38,5 +81,7 @@ int length
             cout<<temp->data<<" ";
             temp=temp->next;
         }
+        cout<<lengthofLL(head);
+        cout<<checkIfPresent(head,15);
 
     }
